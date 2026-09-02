c4e99aa279fcfa97b808215dfea1162b266ee086
Em1lRode <Emilro@uia.no>
2026-09-02 11:16:51 +0200

    Ressurs

---
+++ Prosjekt/Prosjekt/Models/ModelView/RessursViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Prosjekt.Models.ModelView
+{
+    public class RessursViewModel
+    {
+        public string Navn { get; set; } = string.Empty;
+        public string Beskrivelse { get; set; } = string.Empty;
+        public int Antall { get; set; } = 0;
+    }
+}

e056fefcce7d56b42fc72ffb88065a0bf0cfd87e
Emil <emilro@uia.no>
2026-09-10 10:50:45 +0200

    Enhetstesting til ressurs

---
+++ Prosjekt/Prosjekt.Xunit.Test/ControllerTest/RessursControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Prosjekt.Controllers;
+using Prosjekt.Models.ModelView;
+using Xunit;
+
+namespace Prosjekt.Xunit.Test.ControllerTest
+{
+    public class RessursControllerTest
+    {
+        private readonly RessursController _controller;
+
+        public RessursControllerTest()
+        {
+            _controller = new RessursController();
+        }
+
+        [Fact]
+        public void Index_ReturnsViewResultWithModel()
+        {
+            // Act
+            var result = _controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<RessursViewModel>(viewResult.Model);
+        }
+
+        [Fact]
+        public void Create_ValidModel_ReturnsViewResult()
+        {
+            // Arrange
+            var model = new RessursViewModel { Navn = "Test", Beskrivelse = "Test", Antall = 1 };
+
+            // Act
+            var result = _controller.Create(model);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal(model, viewResult.Model);
+        }
+    }
+}
---
+++ Prosjekt/Prosjekt.Xunit.Test/ViewTest/HomeViewTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Prosjekt.Controllers;
+
+namespace Prosjekt.Xunit.Test.ViewTest
+{
+    public class HomeViewTest
+    {
+        [Fact]
+        public void Index_ReturnsViewResult()
+        {
+            // Arrange
+            var controller = new HomeController();
+            // Act
+            var result = controller.Index();
+            // Assert
+            Assert.IsType<Microsoft.AspNetCore.Mvc.ViewResult>(result);
+        }
+        [Fact]
+        public void Index_ReturnsNotNull()
+        {
+            // Arrange
+            var controller = new HomeController();
+            // Act
+            var result = controller.Index();
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
